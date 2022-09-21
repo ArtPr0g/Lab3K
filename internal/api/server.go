1fa2780851e6cbe83d1eb7933fbbe3caa5b02429
ArtPr0g <stebunov2002@mail.ru>
2022-09-21 11:03:03 +0300

    correction slice

--- internal/api/server.go
+++ internal/api/server.go
@@ -2,31 +2,24 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
-	"html/template"
 	"log"
 	"net/http"
 )
 
-type Viewdata struct {
-	Title string
-	Users []Users
-}
 type Users struct {
-	Name    string
-	Release int
-	Grade   float64
+	Id      int     `json:"Id"`
+	Name    string  `json:"Name"`
+	Release int     `json:"Release"`
+	Grade   float64 `json:"Grade"`
 }
 
 func StartServer() {
 	log.Println("Server start up")
 
-	data := Viewdata{
-		Title: "List of films",
-		Users: []Users{
-			Users{Name: "Крепкий орешек", Release: 1988, Grade: 8.0},
-			Users{Name: "Терминатор", Release: 1984, Grade: 8.0},
-			Users{Name: "Побег из Шоушенка", Release: 1994, Grade: 9.1},
-		},
+	var users = []Users{
+		{Id: 1, Name: "Крепкий орешек", Release: 1988, Grade: 8.0},
+		{Id: 2, Name: "Терминатор", Release: 1984, Grade: 8.0},
+		{Id: 3, Name: "Побег из Шоушенка", Release: 1994, Grade: 9.1},
 	}
 
 	r := gin.Default()
@@ -45,17 +38,18 @@ func StartServer() {
 		})
 	})
 
-	r.Static("/image", "./resources")
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
-		tmpl, _ := template.ParseFiles("templates/Users.html")
-		tmpl.Execute(w, data)
+	r.GET("/main", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Фильмы",
+		})
+		c.HTML(http.StatusOK, "Users.html", gin.H{
+			"Users": users,
+		})
 	})
 
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Static("/image", "./resources")
 
-	http.ListenAndServe(":8080", nil)
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 
 	log.Println("Server down")
 }
